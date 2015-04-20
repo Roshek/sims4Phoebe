88eab79aeb638a396cb8eb5b5a59f485d51e731a
gytimi <gy.timi44@gmail.com>
2015-04-20 23:57:16 +0200

    kommentek

--- src/Startup.java
+++ src/Startup.java
@@ -4,33 +4,40 @@ public class Startup {
 	
 	/**\brief Jatek inditasa
 	 * 
-	 * Latrehoz egy új engine-t,
+	 * Latrehoz egy Ãºj engine-t,
 	 * inicializalja es meghivja
 	 * a play() fuggvenyet.
 	 * 
 	 */
 	
-	public void run() {						//KeSZ not sure about this
-		System.out.println("->[:Startup].run()");
+	public void run() {					
+		//System.out.println("->[:Startup].run()");
 		
 		Engine tmp=new Engine();
 		tmp.init(players_num);
 		
 		tmp.play();
 	}
-
-	public int getPlayers_num() {				//KeSZ
-		System.out.println("->[:Startup].getPlayers_num()");
+	
+	/**\brief Getter fgv
+	 * 
+	 * Visszadja a jatekosszamot.
+	 * 
+	 */
+	public int getPlayers_num() {				
+		//System.out.println("->[:Startup].getPlayers_num()");
 		
 		return this.players_num;
 	}
 
-	/**
+	/**\brief Setter
+	 * 
+	 * Beallitja a jatekosszamot.
 	 * 
 	 * @param players_num
 	 */
 	public void setPlayers_num(int players_num) {			//KeSZ
-		System.out.println("->[:Startup].setPlayers_num(players_num)");
+		//System.out.println("->[:Startup].setPlayers_num(players_num)");
 		
 		this.players_num = players_num;
 	}
