c824ab5eafa271352ca9f503b8fdd33fde9d459e
hbalazs11 <hbalazs08@gmail.com>
2015-05-04 13:52:33 +0200

    View váz
    ez lenne itten a View váza, sok szeretettel mindenkibe! :)

---
+++ src/View.java
@@ -0,0 +1,43 @@
+import java.util.List;
+
+
+public class View {
+
+	private Engine engine;
+	private Controller controller;
+	protected Jpanel panel;
+	
+	private List<GraphicMiniRobot> gMiniRobotList;
+	private List<GraphicRobot> gRobotList;
+	private List<GraphicArrow> gArrowList;
+	private List<GraphicOil> gOilList;
+	private List<GraphicSlime> gSlimeList;
+	private GraphicArrow gArrow;
+	
+	public void drawAll(){
+		
+	}
+	
+	
+	public void robotAdded(GraphicRobot gRobot){
+		
+	}
+	
+	public void miniRobotAdded(GraphicMiniRobot gMiniRobot){
+		
+	}
+	
+	public void oilAdded(GraphicOil gOil){
+		
+	}
+	
+	public void slimeAdded(GraphicSlime gSlime){
+		
+	}
+	
+	public void setArrow(){
+		
+	}
+	
+	
+}
