1e64c7eed7836960568b42eb9c926b0e0ba130fb
gytimi <gy.timi44@gmail.com>
2015-05-09 18:11:57 +0200

    draw függvény megírása

--- src/GraphicSlime.java
+++ src/GraphicSlime.java
@@ -5,6 +5,8 @@ public class GraphicSlime implements Drawable{
 
 	private Slime slime;
 	private BufferedImage image;
+	private Graphics g;
+
 	
 	public GraphicSlime(Slime slime){
 		this.slime=slime;
@@ -13,7 +15,14 @@ public class GraphicSlime implements Drawable{
 	
 	public void draw() {
 		if(!slime.getExpired() && slime != null){			//vizsgalat, hogy a ragacs a palyan kell, hogy legyen-e.
-			//TODO
+			//pozicio kinyerese
+			int x = slime.getPos().getX();
+			int y = slime.getPos().getY();
+			// eltolni kell, mert a drawimage a bal sarokhoz igazitja a kepet, nekunk meg a kozeppont van meg
+			if ((x-22)>0) x=x-22; 
+			if ((y-22)>0) y=y-22;
+			//kirajzolas
+			g.drawImage(image, x, y,null);
 		}
 		else{
 			slime=null;									//ha mar nincs a palyan toroljuk a hivatkozast, GC dolgozhat
