3963ed70f5c04f51271c3b164d4d07d0bb852102
gytimi <gy.timi44@gmail.com>
2015-05-09 18:12:57 +0200

    draw függvény megírása

--- src/GraphicRobot.java
+++ src/GraphicRobot.java
@@ -1,3 +1,4 @@
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 
@@ -5,6 +6,8 @@ public class GraphicRobot implements Drawable{
 
 	private Robot robot;
 	private BufferedImage image;
+	private Graphics g;
+
 	
 	public GraphicRobot(Robot robot){
 		this.robot=robot;
@@ -13,7 +16,14 @@ public class GraphicRobot implements Drawable{
 	
 	public void draw() {
 		if(robot.isAlive()){		// Ellenorzes, hogy ki kell e meg rajzolni
-			//TODO
+				//pozicio kinyerese
+				int x = robot.getPosition().getX();
+				int y = robot.getPosition().getY();
+				// eltolni kell, mert a drawimage a bal sarokhoz igazitja a kepet, nekunk meg a kozeppont van meg
+				if ((x-30)>0) x=x-30; 
+				if ((y-30)>0) y=y-30;
+				//kirajzolas
+				g.drawImage(image, x, y,null);
 		}
 		else{
 			//robot=null;								// Itt nem toroljuk a referenciat, tovabbfejleszthetosegi lehetosegek miatt.
