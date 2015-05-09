4c0c2f4ecde94b24c2131e8d632d8ab116f5234a
Prodinal <rapcsak.prodinal@gmail.com>
2015-05-09 16:52:09 +0200

    GamePanelhez háttér hozzáadva
    konstruktor betölti a map képét resourcesból és kirajzolja mindig
    mielőtt a többieket ráfestené

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,6 @@
 import java.awt.Canvas;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
@@ -7,12 +8,18 @@ import javax.swing.JPanel;
 public class GamePanel extends JPanel {
 	
 	View view;
+	BufferedImage background;
 	
 	public GamePanel(View x) {
 		view=x;
+		background=Resources.getMap();
 	}
 	
     protected void paintComponent(Graphics g){
+    	super.paintComponent(g);
+    	
+    	g.drawImage(background, 0, 0, null);
+    	
 		view.drawAll();
 	}
 
