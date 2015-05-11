6d03ee73aa41b54275a182dd8e36c3e0f99ba75d
Prodinal <rapcsak.prodinal@gmail.com>
2015-05-11 10:55:33 +0200

    Resources veglegelve

--- src/Resources.java
+++ src/Resources.java
@@ -12,6 +12,14 @@ public abstract class Resources {
 	private static BufferedImage slime;
 	private static BufferedImage map;
 	
+	
+	/**\Brief betolti a szukseges file-okat
+	 * 
+	 * Minden statikus adattagot inicializal es betolti a szukseges kepeket,
+	 * System.out.println()-nel jelzi ha nem sikerult ez valamiert
+	 * Minden indulasnal betolti mind a negy robot kepet, nem csak a szuksegeseket
+	 * 
+	 */
 	public static void load(){
 		robot=new BufferedImage[5];
 		try {
@@ -23,11 +31,18 @@ public abstract class Resources {
 			slime=ImageIO.read(new File("slime.png"));
 			map=ImageIO.read(new File("map.png"));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			System.out.println(e + "Failed to load images");
+			System.exit(1);
 		}
 	}
-
+	
+	/**\Brief Visszaadja az adott robot kepet
+	 * 
+	 * Elkeri r-tol az ID-jet es visszaadja a robot tomb megfelelo elemet
+	 * 
+	 * @param r
+	 * @return
+	 */
 	public static BufferedImage getRobot(Robot r) {
 		//a kulonbozo jatekosok robotjai kulonbozo szinuek
 		//robot=ImageIO.read(new File("robot"+(r.getID()-1)+".png"));
@@ -35,6 +50,7 @@ public abstract class Resources {
 		//return robot[r.getID()-1];
 		return robot[r.getID()];
 	}
+	
 
 	public static BufferedImage getMiniRobot() {
 		return miniRobot;
