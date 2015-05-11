9707e36d7abb16a21fdb894af7cbc0d171b7bc1f
Prodinal <rapcsak.prodinal@gmail.com>
2015-05-11 02:15:49 +0200

    Mousclicked helyet pressed-re reagál a gamepanel
    More responsive for me this way

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -92,10 +92,11 @@ public class MainWindow extends JFrame {
 		//JATEKTER
 		gameplace.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent arg0) {
+			public void mousePressed(MouseEvent arg0) {
 				if(controller==null)
 					System.out.println("Controller null ptr");
 				controller.setArrow(arg0.getX(), arg0.getY());
+				System.out.println("Mouse clicked on gamepanel");
 			}
 		});
 		
